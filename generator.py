c9ae4d13f82be280d83ccd77a7a3efe24d034a9d
Ruben Partouche <rpartouche@icebergdatalab.com>
2021-12-18 10:24:08 +0100

    generator avec des notes plus régulières (distributions normales)

--- generator.py
+++ generator.py
@@ -1,29 +1,27 @@
 #%%
 import numpy as np
-from random import random
-SCALE = 20
+from random import uniform
 
 class Generator():
-    def __init__(self,size:int,lmbda:float=None,weights:np.ndarray=None,frontier:np.ndarray=None,scale=SCALE,num_criterions = 4) -> None:
+    def __init__(self,size:int,lmbda:float=None,weights:np.ndarray=None,frontier:np.ndarray=None,num_criterions = 4) -> None:
         self.size = size
-        self.scale = scale
         self.lmbda = lmbda
         if lmbda is None:
-            self.lmbda = random()
+            self.lmbda = uniform(0.25,0.75)
         self.weights = weights
         if weights is None:
-            self.weights = np.random.rand(num_criterions)
+            self.weights = np.random.standard_normal(num_criterions)+2
             self.weights /= self.weights.sum()
         self.num_criterions = len(self.weights)
         self.frontier = frontier
         if frontier is None:
-            self.frontier = np.random.rand(self.num_criterions)*SCALE
+            self.frontier = np.random.rand(self.num_criterions)*3+12
 
     def label(self,grades:np.ndarray) -> np.ndarray:
         ok = grades > self.frontier
         return (ok*self.weights).sum(axis=1) > self.lmbda
 
     def generate(self):
-        grades = np.random.rand(self.size,self.num_criterions)*self.scale
-        labels = self.label(grades)
-        return grades,labels
+        grades = np.random.standard_normal((self.size,self.num_criterions))*3+10
+        return grades,self.label(grades)
+# %%
