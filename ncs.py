0832e0fcc365dc9e46b3286faa2406285d7f4c11
Jérôme AUGUSTE <jerome.auguste@student-cs.fr>
2022-01-10 16:39:01 +0100

    FIRST RESULTS ON SAT!!! Need to interpret and check

--- ncs.py
+++ ncs.py
@@ -23,33 +23,33 @@ print(f"Parameters:\nlambda: {gen.lmbda}\nweights: {gen.weights}\nfrontier: {gen
 # - B coalition of criteria (B included in N)
 
 # Generate the indexes of the students belonging to each class as a dict
-students_per_class = {}
-for cat in range(gen.num_classes):
-    students_per_class[cat] = []
-    for student in range (train_set_size):
-            if admission[student] == cat:
-                students_per_class[cat].append(student)
+# students_per_class = {}
+# for cat in range(gen.num_classes):
+#     students_per_class[cat] = []
+#     for student in range (train_set_size):
+#             if admission[student] == cat:
+#                 students_per_class[cat].append(student)
 
 # pp.pprint(students_per_class)
 
 # Generate the indexes of the students per grade for each criterion
-students_per_grade = {}
-for i in range(gen.num_criterions):
-    students_per_grade[i] = {}
-    grades_list = [grades[student][i] for student in range(train_set_size)]
-    grades_set = set(grades_list)
-    for grade in grades_set:
-        students_per_grade[i][grade] = [s for s, g in enumerate(grades_list) if g == grade]
+# students_per_grade = {}
+# for i in range(gen.num_criterions):
+#     students_per_grade[i] = {}
+#     grades_list = [grades[student][i] for student in range(train_set_size)]
+#     grades_set = set(grades_list)
+#     for grade in grades_set:
+#         students_per_grade[i][grade] = [s for s, g in enumerate(grades_list) if g == grade]
 
 # pp.pprint(students_per_grade)
         
 
 # The list format of the boolean value is i + student_index*num_criterias + 1 (in range(1,train_set_size*num_criterias))
-def squeezed_grades_index(student_index: int, i: int) -> int:
-    return i + student_index*gen.num_criterions + 1
+# def squeezed_grades_index(student_index: int, i: int) -> int:
+#     return i + student_index*gen.num_criterions + 1
 
-def squeezed_coalitions_index(coalition_index: int) -> int:
-    return train_set_size*gen.num_criterions + coalition_index
+# def squeezed_coalitions_index(coalition_index: int) -> int:
+#     return train_set_size*gen.num_criterions + coalition_index
 
 def grades_support_per_crit(grades: np.array):
     grades_set = []
@@ -63,9 +63,8 @@ def subsets(criteria: list) -> list:
     x = subsets(criteria[1:])
     return x + [[criteria[0]] + y for y in x]
 
-coalitions = subsets(list(range(gen.num_criterions)))
-
-# pp.pprint(grades_support_per_crit(grades), indent=4, compact=True)
+# Create variables/boolean value encoder
+coalitions = [tuple(el) for el in subsets(list(range(gen.num_criterions)))]
 grades_support = grades_support_per_crit(grades)
 
 variables = {
@@ -73,54 +72,143 @@ variables = {
     "coalition_var": coalitions
     }
 
-pp.pprint(variables)
+front_v2i, front_i2v = dict(), dict()
+front_v2i = {v: i+1 for i, v in enumerate(variables["frontier_var"])}
+front_i2v = {i: v for v, i in front_v2i.items()}
 
-# 3a
+coal_v2i, coal_i2v = dict(), dict()
+coal_v2i = {v: i+len(front_i2v)+1 for i, v in enumerate(variables["coalition_var"])} # Indexes are starting right above where frontier indexing stops
+coal_i2v = {i: v for v, i in coal_v2i.items()}
 
-# Considering the assessed classes in the train_set
+# Create the general index to value dictionnary
+i2v = dict()
+i2v.update(front_i2v)
+i2v.update(coal_i2v)
+
+# 3a Ascending scales
+clauses_3a = []
+
+# Not only adjacent values of k
 # for i in range(gen.num_criterions):
+#     sorted_grades = sorted(grades_support[i])
 #     for h in range(num_classes):
-#         for stud1 in students_per_class[h]:
-#             for stud2 in students_per_class[h]:
-#                 if (grades[stud1][i] < grades[stud2][i]):
-#                     g.add_clause([squeeze_grades(stud1, i), -squeeze_grades(stud2, i)])
+#         for ik in range(len(sorted_grades)):
+#             for ikp in range(ik+1, len(sorted_grades[ik+1:])):
+#                 if sorted_grades[ik] < sorted_grades[ikp]:
+#                     clauses_3a.append([front_v2i[(i, h, sorted_grades[ikp])], -front_v2i[(i, h, sorted_grades[ik])]])
+#                     # print(f"({i}, {h}, {sorted_grades[ikp]}) >  ({i}, {h}, {sorted_grades[ik]})")
 
+# Only for adjacent values of k
 for i in range(gen.num_criterions):
+    sorted_grades = sorted(grades_support[i])
     for h in range(num_classes):
-        for ik in range(len(grades_support[i])):
-            for ikp in range(ik+1, len(grades_support[i])):
-                pass
-    
-# 3b
+        for ik in range(len(sorted_grades)-1):
+            ikp = ik+1
+            while ikp < len(sorted_grades) and sorted_grades[ik] >= sorted_grades[ikp] :
+                ikp += 1
+            if ikp < len(sorted_grades):
+                clauses_3a.append([front_v2i[(i, h, sorted_grades[ikp])], -front_v2i[(i, h, sorted_grades[ik])]])
+                # print(f"({i}, {h}, {sorted_grades[ikp]}) >  ({i}, {h}, {sorted_grades[ik]})")
 
+# 3b Hierarchy of profiles
+clauses_3b = []
+
+# Not only for adjacent values
+# for i in range(gen.num_criterions):
+#     for k in set(grades_support[i]):
+#         for h in range(num_classes-1):
+#             for hp in range(h+1, num_classes):
+#                 clauses_3b.append([front_v2i[(i, h, k)], -front_v2i[(i, hp, k)]])
+#                 # print(f"({i}, {h}, {k}) < ({i}, {hp}, {k})")
+
+# Only for adjacent values
 for i in range(gen.num_criterions):
-    for k in students_per_grade[i].keys():
-        for stud1 in students_per_grade[i][k]:
-            for stud2 in students_per_grade[i][k]:
-                if (admission[stud1] < admission[stud2]):
-                    g.add_clause([squeeze_grades(stud1, i), -squeeze_grades(stud2, i)])
+    for k in set(grades_support[i]):
+        for h in range(num_classes-1):
+            clauses_3b.append([front_v2i[(i, h, k)], -front_v2i[(i, h+1, k)]])
+            # print(f"({i}, {h}, {k}) < ({i}, {h+1}, {k})")
 
-# 3c
-
+# 3c Coalitions strenghs
 
 
+clause_3c = []
 
-for ib in range(len(coalitions)):
-    for ibp in range(len(coalitions)):
-        if set(coalitions[ib]).issubset(set(coalitions[ibp])):
-            g.add_clause(squeeze_coalitions(ibp), - squeeze_coalitions(ib))
+# Not only for "adjacent" coalitions (difference is a singleton)
+for B in coalitions:
+    for Bp in coalitions:
+        if set(B).issubset(set(Bp)):
+            clause_3c.append([coal_v2i[Bp], - coal_v2i[B]])
+            # print(f"{B} is subset of {Bp}")
+            
+# Only for a "adjacent" coalitions
+for B in coalitions:
+    for i in range(gen.num_criterions):
+        Bp = set(B).union(set([i]))
+        if Bp != set(B):
+            clause_3c.append([coal_v2i[tuple(Bp)], - coal_v2i[B]])
+            # print(f"{B} is adjacent subset of {tuple(Bp)}")
 
 
-# 3d
-for ib in range(len(coalitions)):
+# 3d Alternatives are outranked by boundary above them
+students_per_class = [[u for u in range(train_set_size) if admission[u]==h] for h in range(num_classes)] # Students per category
+
+clauses_3d = []
+for B in coalitions:
+    for h in range(1, num_classes):
+        for u in students_per_class[h-1]:
+            clauses_3d.append([-front_v2i[(i, h, grades[u, i])] for i in B ] + [-coal_v2i[B]])
+            # print(f"-{[[(i, h, grades[u, i])] for i in B ]} OR -{B}")
+
+# 3e Alternatives outrank the boundary bellow them
+clauses_3e = []
+for B in coalitions:
     for h in range(num_classes):
-        pass
-# Quelle est la différence entre h et A(u) ? Classe/frontière estimée vs frontière évaluée ? Comment définir la frontière estimée ?
+        for u in students_per_class[h]:
+            N_minus_B = tuple(set(list(range(gen.num_criterions))) - set(B))
+            clauses_3e.append([front_v2i[(i, h, grades[u, i])] for i in B ] + [coal_v2i[N_minus_B]])
+            # print(f"{[[(i, h, grades[u, i])] for i in B ]} OR {N_minus_B}")
+
+
 # Quelles sont les valeurs possibles pour les notes k ? Entier uniquement ?
 
-# g = Glucose3()
-# g.add_clause([-1, 2])
-# g.add_clause([-2, 3])
-# print(g.solve())
-# print(g.get_model())
 
+# ------------------------------------------- TODO: adapt to the problem -------------------------------------------
+#Construction du DIMCS et Résolution
+
+import subprocess
+
+def clauses_to_dimacs(clauses, numvar) :
+    dimacs = 'c SAT NCS encoder \np cnf '+str(numvar)+' '+str(len(clauses))+'\n'
+    for clause in clauses :
+        for atom in clause :
+            dimacs += str(atom) +' '
+        dimacs += '0\n'
+    return dimacs
+
+def write_dimacs_file(dimacs, filename):
+    with open(filename, "w", newline="") as cnf:
+        cnf.write(dimacs)
+
+#Attention à utiliser la vesion du solveur compatible avec votre système d'exploitation, mettre le solveur dans le même dossier que ce notebook        
+
+def exec_gophersat(filename, cmd = "./gophersat.exe", encoding = "utf8") :
+    result = subprocess.run([cmd, filename], stdout=subprocess.PIPE, check=True, encoding=encoding)
+    string = str(result.stdout)
+    lines = string.splitlines()
+
+    if lines[1] != "s SATISFIABLE":
+        return False, [], {}
+
+    model = lines[2][2:].split(" ")
+
+    return True, [int(x) for x in model if int(x) != 0], { i2v[abs(int(v))] : int(v) > 0 for v in model if int(v)!=0} 
+
+
+myClauses = clauses_3a + clauses_3b + clause_3c + clauses_3d + clauses_3e
+myDimacs = clauses_to_dimacs(myClauses, len(variables["frontier_var"]) + len(variables["coalition_var"]))
+
+write_dimacs_file(myDimacs, "workingfile.cnf")
+res = exec_gophersat("workingfile.cnf")
+
+#Résultat
+pp.pprint(res)
