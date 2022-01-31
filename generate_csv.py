8da657744a624274908cd62cdac73d45d7454fcb
Nathan Bruckmann <nathan.bruckmann@student-cs.fr>
2022-01-31 14:47:32 +0100

    single sat, maxsat, check generator

--- generate_csv.py
+++ generate_csv.py
@@ -1,6 +1,8 @@
 from time import time
 from tools.generator import Generator
 from tools.csvReader import csvReader
+from single_peak_sat import SinglePeakModel
+from single_peak_maxsat import MaxSatSinglePeakModel
 from mrsort import MRSort
 from tools.parseArg import parseArguments
 from tools.utils import print_comparison
@@ -14,9 +16,18 @@ if __name__=='__main__':
     df = pd.DataFrame()
     for iter, size, noise, num_classes, num_criteria in product(range(5), range(25, 101, 25), range(0,16,5), range(2,5), range(3,7)):
         noisy = (noise > 0)
-        gen = Generator(
-            size, num_classes=num_classes, num_criteria=num_criteria, noisy=noisy, noise_percent=noise/100
-        )
+
+        # Check that all classes are represented
+        gen_ok = False
+        gen = None
+        while not gen_ok:
+            gen_ok = True
+            gen = Generator(
+                size, num_classes=num_classes, num_criteria=num_criteria, noisy=noisy, noise_percent=noise/100
+            )
+            for i in range(num_classes):
+                if i not in gen.admission:
+                    gen_ok = False
 
         # MR_Sort
         mr_perf = {}
@@ -55,5 +66,48 @@ if __name__=='__main__':
         ncs_perf["accuracy_on_train"] = accuracy(train_labels, gen.admission)
         ncs_perf["accuracy_on_test"] = accuracy(test_labels, gen.admission_test)
         df = df.append(ncs_perf, ignore_index=True)
+
+        # Single Peak Model
+        spm_perf = {}
+        spm_begin = time()
+        u_spm = SinglePeakModel(generator=gen)
+        
+        u_spm.set_gophersat_path(args.gopher_path)
+        train_labels = u_spm.train()
+        spm_end = time()
+        test_labels = u_spm.predict()
+
+        spm_perf["name"] = "NCS-SinglePeak"
+        spm_perf["size"] = size
+        spm_perf["noise"] = noise/100
+        spm_perf["num_classes"] = num_classes
+        spm_perf["num_criteria"] = num_criteria
+        spm_perf["time"] = spm_end - spm_begin
+        spm_perf["accuracy_on_train"] = accuracy(train_labels, gen.admission)
+        spm_perf["accuracy_on_test"] = accuracy(test_labels, gen.admission_test)
+        df = df.append(spm_perf, ignore_index=True)
+
+        # MaxSAT Single Peak Model
+        maxsat_perf = {}
+        spm_begin = time()
+        u_maxsat = MaxSatSinglePeakModel(generator=gen)
+
+        print(iter, size, noise, num_classes, num_criteria)
+        
+        u_maxsat.set_gophersat_path(args.gopher_path)
+        train_labels = u_maxsat.train()
+        spm_end = time()
+        test_labels = u_maxsat.predict()
+
+        maxsat_perf["name"] = "NCS-MaxSAT"
+        maxsat_perf["size"] = size
+        maxsat_perf["noise"] = noise/100
+        maxsat_perf["num_classes"] = num_classes
+        maxsat_perf["num_criteria"] = num_criteria
+        maxsat_perf["time"] = spm_end - spm_begin
+        maxsat_perf["accuracy_on_train"] = accuracy(train_labels, gen.admission)
+        maxsat_perf["accuracy_on_test"] = accuracy(test_labels, gen.admission_test)
+        df = df.append(maxsat_perf, ignore_index=True)
+
     df_mean = df.groupby(['name', 'size', 'noise', 'num_classes', 'num_criteria']).mean()
     df_mean.to_csv('results.csv')
